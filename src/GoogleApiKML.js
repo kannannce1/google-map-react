1fc94ecd31ff2b4934acbff43a9cff2e0cf6f7ce
kannannce1 <48784824+kannannce1@users.noreply.github.com>
2020-07-23 10:16:48 +0530

    kml to draw shape using polygon

--- src/GoogleApiKML.js
+++ src/GoogleApiKML.js
@@ -8,7 +8,7 @@ import toGeoJSON from '@mapbox/togeojson';
 
 
 
-
+var mmm=[]
 var onGoogleApiLoaded = (map, maps) => {
   var src = 'https://developers.google.com/maps/documentation/javascript/examples/kml/westcampus.kml';
   //var KMZUrl = 'http://iblogbox.github.io/js/gpx/sample/Blackbirds.kmz';
@@ -21,37 +21,60 @@ var onGoogleApiLoaded = (map, maps) => {
     //console.log("working")
   //});
 
-
- 
+  axios.get(src).then(res => {
+    console.log(res.data, '....');
+   // console.log(toGeoJSON.kml(res));
+  var  parser = new DOMParser();
+  var  xmlDoc = parser.parseFromString(res.data,"text/xml");
+  const coordinatesObj = xmlDoc.getElementsByTagName("coordinates");
   
-  var triangleCoords = [
-    { lat: 25.774, lng: -80.19 },
-    { lat: 18.466, lng: -66.118 },
-    { lat: 32.321, lng: -64.757 },
-    { lat: 25.774, lng: -80.19 }
-  ];
+    console.log(typeof(coordinatesObj))
+    console.log(coordinatesObj.length)
+    var mmm=[]
+    for (var i = 0; i < coordinatesObj.length; i++) {
+      var name = coordinatesObj[i].firstChild.nodeValue;
+      mmm.push({lat: parseFloat(name.split(",")[1]),lng:parseFloat(name.split(",")[0])})
+     // var div = document.createElement("div");
+      //var textNode = document.createTextNode(name);
+      //div.appendChild(textNode);
+      //document.getElementById("wrapper").appendChild(div);
+    }
+    console.log(JSON.stringify(mmm))
 
-  // Construct the polygon.
-  var bermudaTriangle = new maps.Polygon({
-    paths: triangleCoords,
-    strokeColor: "#FF0000",
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: "#FF0000",
-    fillOpacity: 0.35
+  //var coordinates = xmlDoc.getElementsByTagName("coordinates")[0].childNodes[0].nodeValue;
+  //console.log('coordinates', coordinates)
+
+
+    // Construct the polygon.
+    var bermudaTriangle = new maps.Polygon({
+      paths: mmm,
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35
+    });
+   bermudaTriangle.setMap(map);
+
+
+
+
   });
- // bermudaTriangle.setMap(map);
 
-  var kmlLayer = new maps.KmlLayer(src, {
+  
+  console.log('mmm',mmm[0])
+
+  /*var kmlLayer = new maps.KmlLayer(src, {
     suppressInfoWindows: true,
     preserveViewport: false,
     map: map
-  });
+  });*/
 };
 
 
-/*
-const GoogleApiKML = () => (
+
+const GoogleApiKML = (mmm) => (
+  
   <GoogleMapReact
     defaultCenter={{ lat: 24.886, lng: -70.268 }}
     defaultZoom={5}
@@ -59,19 +82,12 @@ const GoogleApiKML = () => (
     onGoogleApiLoaded={({ map, maps }) => onGoogleApiLoaded(map, maps)}
   />
 );
-*/
-class GoogleApiKML extends Component {
+
+/*class GoogleApiKML extends Component {
 
   componentDidMount(){
-    let url = 'https://developers.google.com/maps/documentation/javascript/examples/kml/westcampus.kml';
-    axios.get(url).then(res => {
-      console.log(res.data, '....');
-     // console.log(toGeoJSON.kml(res));
-    var  parser = new DOMParser();
-    var  xmlDoc = parser.parseFromString(res.data,"text/xml");
-    var coordinates = xmlDoc.getElementsByTagName("coordinates")[0].childNodes[0].nodeValue;
-    console.log('coordinates', coordinates)
-    });
+   // let url = 'https://developers.google.com/maps/documentation/javascript/examples/kml/westcampus.kml';
+   
    }
   
 
@@ -86,5 +102,5 @@ render() {
   )
 }
 };
-
+*/
 export default GoogleApiKML;
--- src/GoogleApiPolygon.js
+++ src/GoogleApiPolygon.js
@@ -3,11 +3,21 @@ import GoogleMapReact from "google-map-react";
 
 const onGoogleApiLoaded = (map, maps) => {
   var triangleCoords = [
-    { lat: 25.774, lng: -80.19 },
+    /*{ lat: 25.774, lng: -80.19 },
     { lat: 18.466, lng: -66.118 },
     { lat: 32.321, lng: -64.757 },
-    { lat: 25.774, lng: -80.19 }
+    { lat: 25.774, lng: -80.19 }*/
+    
+    /*{lat:-122.0914977709329,lng:37.42390182131783},
+    {lat:-122.0926995893311,lng:37.42419403634421},
+    {lat:-122.0922532985281,lng:37.42301710721216}*/
+
+    {lat:37.42390182131783,lng:-122.0914977709329},
+    {lat:37.42419403634421,lng:-122.0926995893311},
+    {lat:37.42301710721216,lng:-122.0922532985281}
   ];
+
+  
 
   // Construct the polygon.
   var bermudaTriangle = new maps.Polygon({
@@ -23,7 +33,7 @@ const onGoogleApiLoaded = (map, maps) => {
 
 const GoogleApiPolygon = () => (
   <GoogleMapReact
-    defaultCenter={{ lat: 24.886, lng: -70.268 }}
+    defaultCenter={{ lat: 37.42390182131783, lng: -122.0914977709329 }}
     defaultZoom={5}
     yesIWantToUseGoogleMapApiInternals
     onGoogleApiLoaded={({ map, maps }) => onGoogleApiLoaded(map, maps)}
